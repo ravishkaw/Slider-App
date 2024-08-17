6c43f1fd527fd3804b41be3869ca833272b2f794
Ravishka <127718357+ravishkaw@users.noreply.github.com>
2024-08-17 19:19:31 +0530

    update app to use slick carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 
 import Carousel from "./components/Carousel";
+import SlickCarousel from "./components/SlickCarousel";
 
 const App = () => {
   return (
     <>
-      <Carousel />
+      {/* <Carousel /> */}
+      <SlickCarousel/>
     </>
   );
 };
